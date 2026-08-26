0976ebaba1c0f2763c984cdb0ff5c9740d66a792
pele1610 <pelecolo56@gmail.com>
2026-08-26 14:31:55 +1000

    feat: add Community schemas for output, create, and update

---
+++ server/app/schemas/community_schema.py
@@ -0,0 +1,33 @@
+from marshmallow import fields, validate
+
+from app.extensions import ma
+from app.models.communities import Community
+
+
+class CommunitySchema(ma.SQLAlchemyAutoSchema):
+	class Meta:
+		model = Community
+		load_instance = False
+
+
+class CommunityCreateSchema(ma.SQLAlchemyAutoSchema):
+	name = fields.String(required=True, validate=validate.Length(min=1, max=255))
+
+	class Meta:
+		model = Community
+		load_instance = False
+		fields = ("name", "description", "category", "location")
+
+
+class CommunityUpdateSchema(ma.SQLAlchemyAutoSchema):
+	class Meta:
+		model = Community
+		load_instance = False
+		partial = True
+		fields = ("name", "description", "category", "location")
+
+
+community_schema = CommunitySchema()
+communities_schema = CommunitySchema(many=True)
+community_create_schema = CommunityCreateSchema()
+community_update_schema = CommunityUpdateSchema()
