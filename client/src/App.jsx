7be9426f7ee6adb60eaf904cfc7c5cb2a9084285
theblackkingshow <ckipchirchirasher@gmail.com>
2026-08-28 08:44:19 +0300

    Revert "fix: correct homepage import casing"
    This reverts commit 88cb1dc875e1736689eef3fdcebe28a55c7eec55.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import HomePage from "./HomePage";
+import HomePage from "./Homepage";
 import GetHelpPage from "./pages/GetHelpPage";
 import "./App.css";
 
