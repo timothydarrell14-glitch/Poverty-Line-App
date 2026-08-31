577bfbe7296a9dcb3a98c1dd121c03008aee0b0a
TimDev14 <njagi.timothy14@gmail.com>
2026-08-31 09:11:28 +0300

    fix: comment out unused JobsPlaceholder route in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
           <Route path="/donors" element={<DonorsPage />} />
 
-          <Route path="/jobs" element={<JobsPlaceholder />} />
+          {/* <Route path="/jobs" element={<JobsPlaceholder />} /> */}
           {/* Logistics Page */}
           <Route
             path="/logistics"
