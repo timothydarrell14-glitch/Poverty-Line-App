5a6a474ee989f4201405b6d6d9555d9be5d4a6d7
TimDev14 <njagi.timothy14@gmail.com>
2026-08-31 14:11:50 +0300

    configured port to run on 5176

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: 'localhost',
+    port: 5176,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5000',
