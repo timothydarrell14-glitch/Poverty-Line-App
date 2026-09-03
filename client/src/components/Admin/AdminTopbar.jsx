59b0f19c31330276568214b0da9ec4129d454acc
TimDev14 <njagi.timothy14@gmail.com>
2026-09-03 10:19:10 +0300

    fixed linting and importation issue

--- client/src/components/Admin/AdminTopbar.jsx
+++ client/src/components/Admin/AdminTopbar.jsx
@@ -14,6 +14,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAdminSession } from "../AdminSession";
 import { useHelpTour } from "../../utils/useHelpTour";
 import { formatRelativeTime } from "../../utils/formatRelativeTime";
+import { useTheme } from "../../context/ThemeContext";
 import {
   listNotifications,
   markAllNotificationsRead,
